5947063e704be4163b5fd84cc7fa2481eef6d762
Robert Matthees <robertmatthees@users.noreply.github.com>
2017-05-23 17:27:47 +0200

    Update ga-rm.js

--- ga-rm.js
+++ ga-rm.js
@@ -19,22 +19,22 @@ $(function() {
  //default settings
  //elements triggered on: focusin keypress (start) & blur (stop)
  //*tracked input types -> input[type="LIST_THIS_BELOW"]
- var ga_rm_input_types = "text, password, color, date, datetime-local, email, file, month, number, search, tel, time, url, week";
+ var ga_rm_input_types="text, password, color, date, datetime-local, email, file, month, number, search, tel, time, url, week";
  //*edit just till: input[type=", entries afterwards are generated automatically using the input type list above
- var ga_rm_elements = 'textarea, input[type="'+ga_rm_input_types.replace(/ /g, "").split(",").join('"], input[type="')+', .ga-rm-focus:not(.ga-rm-hover)"]';
+ var ga_rm_elements='textarea, input[type="'+ga_rm_input_types.replace(/ /g, "").split(",").join('"], input[type="')+', .ga-rm-focus:not(.ga-rm-hover)"]';
 
  //elements triggered on: mouseenter (start) & mouseleave change (stop)
- var ga_rm_hover_elements = 'label, select, input[type="checkbox"], input[type="radio"], input[type="range"], .ga-rm-hover:not(.ga-rm-focus)';
+ var ga_rm_hover_elements='label, select, input[type="checkbox"], input[type="radio"], input[type="range"], .ga-rm-hover:not(.ga-rm-focus)';
 
  //init counter
- var ga_rm_counter = 0;
+ var ga_rm_counter=0;
 
  //function startcount
- var ga_rm_startcount = function() {
+ var ga_rm_startcount=function() {
 
   //clear interval, get element
   ga_rm_stopcount();
-  $el = $(this);
+  $el=$(this);
 
   //check if element is a <label>
   if($el.is('label')) {
@@ -54,33 +54,33 @@ $(function() {
   if(typeof($el.data('ga_rm_time'))=="undefined") { $el.data('ga_rm_time', 0); }
 
   //start counter
-  ga_rm_counter = setInterval(function() {
+  ga_rm_counter=setInterval(function() {
    $el.data('ga_rm_time', $el.data('ga_rm_time')+0.1);
   }, 100);
  }
 
  //function stopcount
- var ga_rm_stopcount = function() {
+ var ga_rm_stopcount=function() {
   clearInterval(ga_rm_counter);
  }
 
  //function trackcount
- var ga_rm_trackcount = function() {
+ var ga_rm_trackcount=function() {
 
   //create array
-  var tracked = [];
+  var tracked=[];
   $(ga_rm_elements).each(function() {
-   var this_row = {}; $el = $(this);
+   var this_row={}; $el=$(this);
    if($el.data('ga_rm_time')>0.1) {
-    if($el.prop('id')!="") { this_row[0] = $el.prop('id'); }
-    else { this_row[0] = $el.attr('name'); }
-    this_row[1] = $el.data('ga_rm_time');
+    if($el.prop('id')!="") { this_row[0]=$el.prop('id'); }
+    else { this_row[0]=$el.attr('name'); }
+    this_row[1]=$el.data('ga_rm_time');
     tracked.push(this_row);
    }
   });
 
   //sort array desc
-  tracked.sort(function(a, b) { return b[1] - a[1]; });
+  tracked.sort(function(a, b) { return b[1]-a[1]; });
 
   //send events to google analytics
   for(i in tracked) {
@@ -89,7 +89,7 @@ $(function() {
  }
 
  //function tracksingle (time on page)
- var ga_rm_tracksingle = function(event) {
+ var ga_rm_tracksingle=function(event) {
   ga('send', 'event', 'user interaction', event.type, $(this).prop('id'), parseInt((event.timeStamp/1000).toFixed(1)));
  }
 
